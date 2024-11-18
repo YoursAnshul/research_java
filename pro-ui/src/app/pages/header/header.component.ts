e8e6ee3e0a94d1253d0adca3431dd2cd6d472ea6
Anshul Upadhyay <hgupta@threecolts.com>
2024-11-18 22:47:12 +0530

    fix issue

--- pro-ui/src/app/pages/header/header.component.ts
+++ pro-ui/src/app/pages/header/header.component.ts
@@ -18,36 +18,43 @@ export class HeaderComponent implements OnInit {
     'This is a brief description of Project Q. It will give you a quick overview of what the study is about. This might be helpful for when interviewers take incoming calls from participants inquiring about studies on which the interviewer isn’t trained.';
   description5: string =
     'This is a brief description of Purple Project. It will give you a quick overview of what the study is about. This might be helpful for when interviewers take incoming calls from participants inquiring about studies on which the interviewer isn’t trained.';
+    description6: string =
+    'This is a brief description of XYZ Trial. It will give you a quick overview of what the study is about. This might be helpful for when interviewers take incoming calls from participants inquiring about studies on which the interviewer isn’t trained.';
 
   firstHeading = 'HERO Together';
   secondHeading = 'Project 42';
   thirdHeading = 'Project Eleven';
   fourtHeading = 'Project Q';
   fifthHeading = 'Purple Project';
+  sixthHeading = 'XYZ Trial';
 
   tollFree1 = 'Toll-Free Phone #';
   tollFree2 = 'Toll-Free Phone #';
   tollFree3 = 'Toll-Free Phone #';
   tollFree4 = 'Toll-Free Phone #';
   tollFree5 = 'Toll-Free Phone #';
+  tollFree6 = 'Toll-Free Phone #';
 
   tollFreeNum1 = '1-800-999-8888';
   tollFreeNum2 = '1-800-999-8888';
   tollFreeNum3 = '1-800-999-8888';
   tollFreeNum4 = '1-800-999-8888';
   tollFreeNum5 = '1-800-999-8888';
+  tollFreeNum6 = '1-800-999-8888';
 
   email1 = 'Email Address';
   email2 = 'Email Address';
   email3 = 'Email Address';
   email4 = 'Email Address';
   email5 = 'Email Address';
+  email6 = 'Email Address';
 
   emailAddress1 = 'herotogether@duke.com';
   emailAddress2 = 'email1@inbox.com';
   emailAddress3 = 'email1@inbox.com';
   emailAddress4 = 'email1@inbox.com';
   emailAddress5 = 'email1@inbox.com';
+  emailAddress6 = 'email1@inbox.com';
 
   searchTerms: string = '';
   authenticatedUser!: IAuthenticatedUser;
