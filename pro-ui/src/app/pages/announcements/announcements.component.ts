9eda73a208923874a2dade66e5f590bed4eb9c05
Kanha Rathod <rathodkanha8810@gmail.com>
2025-01-06 23:24:39 +0530

    working on feedback point

--- pro-ui/src/app/pages/announcements/announcements.component.ts
+++ pro-ui/src/app/pages/announcements/announcements.component.ts
@@ -24,6 +24,7 @@ export class AnnouncementsComponent implements OnInit {
   announcementsList: Announcement[] = [];
   maxLength = 100;
   userObj: any;
+  allProjectList: any[] = [];
   constructor(
     private http: HttpClient,
     private authenticationService: AuthenticationService
@@ -32,10 +33,11 @@ export class AnnouncementsComponent implements OnInit {
     this.authenticationService.authenticatedUser.subscribe(
       (authenticatedUser) => {
         this.authenticatedUser = authenticatedUser;
-        this.userObj = this.authenticatedUser;        
+        this.userObj = this.authenticatedUser;
       }
     );
     this.getAnnouncementList();
+    this.getProjectInfo();
   }
   getAnnouncementList(): void {
     const apiUrl = `${environment.DataAPIUrl}/manage-announement/list`;
@@ -45,7 +47,7 @@ export class AnnouncementsComponent implements OnInit {
           data?.Subject?.map((item: any) => ({
             ...item,
             isFullText: false,
-          })) || [];
+          })) || [];        
       },
       error: (error: any) => {
         console.error('Error fetching announcements:', error);
@@ -64,5 +66,36 @@ export class AnnouncementsComponent implements OnInit {
 
   toggleText(announcement: Announcement): void {
     announcement.isFullText = !announcement.isFullText;
+  }
+  getHighlightedTextParts(text: string): { text: string; isLink: boolean }[] {
+    const regex = /(\b[\w.-]+(?:\.[a-z]{2,})\b)/g;
+    let match;
+    let lastIndex = 0;
+    const parts = [];
+
+    while ((match = regex.exec(text)) !== null) {
+      if (match.index > lastIndex) {
+        parts.push({ text: text.slice(lastIndex, match.index), isLink: false });
+      }
+      parts.push({ text: match[0], isLink: true });
+      lastIndex = regex.lastIndex;
+    }
+
+    if (lastIndex < text.length) {
+      parts.push({ text: text.slice(lastIndex), isLink: false });
+    }
+
+    return parts;
+  }
+  getProjectInfo(): void {
+    const apiUrl = `${environment.DataAPIUrl}/manage-announement/projects`;
+    this.http.get(apiUrl).subscribe({
+      next: (data: any) => {
+        this.allProjectList = data ? data : [];
+      },
+      error: (error: any) => {
+        console.error('Error fetching project info:', error);
+      },
+    });
   }
 }
