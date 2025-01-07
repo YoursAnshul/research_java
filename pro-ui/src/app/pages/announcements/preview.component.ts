773bd95ab13ada6a4660c44a3242114162beb9ab
Anshul Upadhyay <hgupta@threecolts.com>
2025-01-07 23:26:41 +0530

    fix issue

--- pro-ui/src/app/pages/announcements/preview.component.ts
+++ pro-ui/src/app/pages/announcements/preview.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
@@ -11,7 +11,7 @@ export class PreviewComponent implements OnInit {
   previewobj: any;
   constructor(
     public dialogRef: MatDialogRef<PreviewComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,private sanitizer: DomSanitizer
   ) {
     this.previewobj = data;    
   }
@@ -30,4 +30,14 @@ export class PreviewComponent implements OnInit {
   isArray(value: any): boolean {
     return Array.isArray(value);
   }
+    getSafeHtml(htmlString: string): SafeHtml {
+      console.log("htmlString---------- ", htmlString);
+    
+      if (!htmlString) return '';
+        const updatedHtmlString = htmlString.replace(/<a\s+(.*?)href="(?!https?:\/\/)(.*?)"/g, (match, preAttributes, url) => {
+        return `<a ${preAttributes}href="https://${url}"`;
+      });
+    
+      return this.sanitizer.bypassSecurityTrustHtml(updatedHtmlString);
+    }
 }
