247d8cff60f1b584d7efe3476b6395ff78c2993a
Kanha Rathod <rathodkanha8810@gmail.com>
2025-01-09 00:17:20 +0530

    fixed

--- pro-ui/src/app/pages/announcements/add-announcement-dialog.component.ts
+++ pro-ui/src/app/pages/announcements/add-announcement-dialog.component.ts
@@ -287,7 +287,6 @@ export class AddAnnouncementDialogComponent implements OnInit {
     this.dialogRef.close();
   }
   openPreview(): void {
-    this.closeDialog();
     this.announcement.content = this.quill.root.innerHTML;
     const plainTextContent = this.quill.root.textContent;
     const selectedAuthorName = this.selectedAuthor?.userName;
@@ -300,16 +299,9 @@ export class AddAnnouncementDialogComponent implements OnInit {
         ? 'Any Projects'
         : this.selectedProjects,
     };
-    const previewDialogRef = this.dialog.open(PreviewComponent, {
+    this.dialog.open(PreviewComponent, {
       width: '600px',
       data: announcementData,
-    });
-  
-    previewDialogRef.afterClosed().subscribe(() => {
-      this.dialog.open(AddAnnouncementDialogComponent, {
-        width: '600px',
-        data: this.announcementData, 
-      });
     });
   }
 
