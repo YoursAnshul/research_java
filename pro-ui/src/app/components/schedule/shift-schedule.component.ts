ef9c95a52c0f2f1b168eb5a1c0ca98c2581b1ca7
Kanha Rathod <kanha@sphinx-solution.com>
2025-08-19 13:32:21 -0400

    fixed

--- pro-ui/src/app/components/schedule/shift-schedule.component.ts
+++ pro-ui/src/app/components/schedule/shift-schedule.component.ts
@@ -2232,7 +2232,7 @@ export class ShiftScheduleComponent implements OnInit {
     const shift = this.shiftForm.value || {};
     this.http
       .delete(
-        `${environment.DataAPIUrl}/api/userSchedules/delete-schedule/${shift.id}`
+        `${environment.DataAPIUrl}/api/userSchedules/delete-schedule/${shift.id}/${this.authenticatedUser.netID}`
       )
       .subscribe({
         next: (res: any) => {
