748d40eaf883d19aef93b9cb0e09a0cce483e610
Anshul Upadhyay <126473911+anshul-threecolts@users.noreply.github.com>
2025-08-28 10:18:05 -0400

    remove amazon bot code

--- Dimetyd_Amazon_Bot/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
-export PATH=$JAVA_HOME/bin:$PATH
-export DISPLAY=:99
-dbus-launch --exit-with-session Xvfb :99 -screen 0 1024x768x24 &
-sleep 2 & fluxbox & x11vnc -display :99 -N -forever & chromium-browser --no-sandbox & java -jar /app/dfo.jar $1
